4cac1bb6e0ee6e4d1a32701355bb1f25ac5e31f7
Bew2000 <146051593+Bew2000@users.noreply.github.com>
2024-02-27 15:04:10 +0700

    27/02/2024

--- Controllers/deliveryController/๋J&T/priceWeight.control.js
+++ Controllers/deliveryController/๋J&T/priceWeight.control.js
@@ -2,12 +2,34 @@ const { priceWeight } = require("../../../Models/Delivery/J&T/priceWeight");
 
 createWeight = async(req, res)=>{
     try{
-        const weight_s = req.body.weight_s
-        const weight_e = req.body.weight_e
+        const weight = req.body.weight
         const price = req.body.price
+        const findWeight = await priceWeight.findOne({weight: weight})
+            if(findWeight){
+                return res
+                        .status(400)
+                        .send({status:false, message:"มีน้ำหนักนี้ในระบบแล้ว"})
+            }
+        const create = await priceWeight.create(
+            {
+                weight: weight,
+                price: price
+            }
+        )
+            if(!create){
+                return res
+                        .status(400)
+                        .send({status:false, message:"ไม่สามารถสร้างข้อมูลได้"})
+            }
+        return res
+                .status(200)
+                .send({status:true, data: create})
     }catch(err){
         return res
                 .status(500)
                 .send({status:false, message:err})
     }
 }
+
+
+module.exports = { createWeight }
