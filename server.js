164bedaab999b12be63cd1b832ba9bd4514afb5c
gg <crowsnop4@gmail.com>
2024-07-04 17:33:47 +0700

    04/07/2024

--- server.js
+++ server.js
@@ -3,18 +3,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require("cors");
 var router = require('./route/route')
 var router2 = require('./route/route2')
-
-app.use(cors());
+const cors = require("cors");
 
 app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DB)
 .then(() => console.log('Connected!'));
-const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
