e46cda8327e09d16e219a641725d27faa02c7b4a
gg <crowsnop4@gmail.com>
2024-07-12 11:31:52 +0700

    12/07/2024

--- server.js
+++ server.js
@@ -28,7 +28,13 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
+  if (req.method === 'OPTIONS') {
+    console.log('Handling OPTIONS request');
+    res.sendStatus(200);
+  } else {
+    console.log('Handling regular request: ' + req.method);
+    next();
+  }
 });
 
 app.use(helmet.contentSecurityPolicy({
